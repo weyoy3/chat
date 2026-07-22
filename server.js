3908a6a29d81dcf07d8f3e909a9276207ff808f4
weyoy3 <weyoy3@gmail.com>
2026-07-23 01:07:26 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static(__dirname));
+
+let waitingUser = null;
+
+io.on('connection', (socket) => {
+    console.log('مستخدم متصل:', socket.id);
+
+    socket.on('find_partner', () => {
+        if (waitingUser && waitingUser.id !== socket.id) {
+            const partner = waitingUser;
+            waitingUser = null;
+
+            const room = 'room_' + partner.id + '_' + socket.id;
+            socket.join(room);
+            partner.join(room);
+
+            io.to(room).emit('matched');
+            socket.roomName = room;
+            partner.roomName = room;
+        } else {
+            waitingUser = socket;
+        }
+    });
+
+    socket.on('message', (msg) => {
+        if (socket.roomName) {
+            socket.to(socket.roomName).emit('message', msg);
+        }
+    });
+
+    socket.on('disconnect', () => {
+        if (waitingUser === socket) {
+            waitingUser = null;
+        }
+        if (socket.roomName) {
+            socket.to(socket.roomName).emit('partner_disconnected');
+        }
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log('Server running on port ' + PORT);
+});
