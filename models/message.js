277568c1c8cf09bc8792cd2bad6b150aea9ce84e
weyoy3 <weyoy3@gmail.com>
2026-07-25 02:32:18 +0300

    Update message.js

--- models/message.js
+++ models/message.js
@@ -1,1 +1,130 @@
 
+const mongoose = require("mongoose");
+
+const messageSchema = new mongoose.Schema({
+
+    senderId:{
+
+        type:mongoose.Schema.Types.ObjectId,
+
+        ref:"User",
+
+        required:true
+
+    },
+
+    username:{
+
+        type:String,
+
+        required:true
+
+    },
+
+    avatar:{
+
+        type:String,
+
+        default:"avatars/default.png"
+
+    },
+
+    memberType:{
+
+        type:String,
+
+        default:"عضو"
+
+    },
+
+    text:{
+
+        type:String,
+
+        required:true,
+
+        trim:true,
+
+        maxlength:5000
+
+    },
+
+    room:{
+
+        type:String,
+
+        default:"general"
+
+    },
+
+    messageType:{
+
+        type:String,
+
+        enum:[
+
+            "text",
+
+            "system"
+
+        ],
+
+        default:"text"
+
+    },
+
+    isEdited:{
+
+        type:Boolean,
+
+        default:false
+
+    },
+
+    editedAt:{
+
+        type:Date,
+
+        default:null
+
+    },
+      isDeleted:{
+
+        type:Boolean,
+
+        default:false
+
+    },
+
+    deletedBy:{
+
+        type:mongoose.Schema.Types.ObjectId,
+
+        ref:"User",
+
+        default:null
+
+    },
+
+    deletedAt:{
+
+        type:Date,
+
+        default:null
+
+    }
+
+},
+{
+
+    timestamps:true
+
+});
+
+module.exports = mongoose.model(
+
+    "Message",
+
+    messageSchema
+
+);
